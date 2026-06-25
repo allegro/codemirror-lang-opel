df8c422416b730c030b012a856954f98a0e706ec
Jakub Jedlikowski <jakub.jedlikowski@allegro.com>
2026-06-25 14:06:28 +0200

    fix package build

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,14 @@
+import path from 'node:path';
 import typescript from '@rollup/plugin-typescript';
 import { lezer } from '@lezer/generator/rollup';
 
 export default {
   input: 'src/index.ts',
-  external: (id) => id != 'tslib' && !/^(\.?\/|\w:)/.test(id),
+  external: (id) =>
+    id !== 'tslib' &&
+    !id.startsWith('\0') &&
+    !id.startsWith('.') &&
+    !path.isAbsolute(id),
   output: [
     { file: 'dist/index.cjs', format: 'cjs' },
     { dir: './dist', format: 'es' },
